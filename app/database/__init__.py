d62a83986c23a16bac1dcef2c9a0118c44e7aa58
Saranwich <saranwichpochai.idea@gmail.com>
2026-04-15 20:47:46 +0700

    fix(db): ensure JSON serialization supports Thai characters

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,13 +1,14 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import declarative_base
 from app.config import DATABASE_URL
+import json
 
 # SQLAlchemy async with PostgreSQL requires 'asyncpg' driver instead of the default one
 # We replace 'postgresql://' with 'postgresql+asyncpg://' automatically
 ASYNC_DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
 
 # create_async_engine is the async version of create_engine
-engine = create_async_engine(ASYNC_DATABASE_URL, echo=False)
+engine = create_async_engine(ASYNC_DATABASE_URL, echo=False,json_serializer=lambda obj: json.dumps(obj, ensure_ascii=False))
 
 # async_sessionmaker returns an AsyncSession
 SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
